7e9f56207f4c4369449e71cd4e440115609dee8c
Connor Hammond <ConnorH2582@users.noreply.github.com>
2015-06-09 12:11:40 -0400

    Adds API wrapper methods for gameday pickem for each game; shows matchups, scores, winners, & losers

--- tripleFapi/fballAPI/urls.py
+++ tripleFapi/fballAPI/urls.py
@@ -1,12 +1,61 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-# from users.views import 
+from fballAPI.views import AllQuarterbacksView, AllRunningbacksView, AllReceiversView, AllTightEndsView, AllKickersView, AllLinemenView, AllLinebackersView, AllDefensiveBacksView, AllPuntersView, QuarterbackView, RunningbackView, ReceiverView, TightEndView, KickerView, LinemanView, LinebackerView, DefensiveBackView, PunterView,WeeklyScheduleView,WeeklyScoresView, WeeklyWinnersView,WeeklyLosersView
 from django.views.generic import View
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tripleFapi.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^/', ),
 
+    # all positional offensive player stats in season
+    url(r'^quarterbacks/(?P<year>20[0-2][0-9])$', AllQuarterbacksView.as_view()),
+
+    url(r'^runningbacks/(?P<year>20[0-2][0-9])/$', AllRunningbacksView.as_view()),
+
+    url(r'^receivers/(?P<year>20[0-2][0-9])/$', AllReceiversView.as_view()),
+
+    url(r'^tightends/(?P<year>20[0-2][0-9])/$', AllTightEndsView.as_view()),
+
+    url(r'^kickers/(?P<year>20[0-2][0-9])/$', AllKickersView.as_view()),
+
+    # all positional defensive player stats in season
+    url(r'^linemen/(?P<year>20[0-2][0-9])/$', AllLinemenView.as_view()),
+
+    url(r'^linebackers/(?P<year>20[0-2][0-9])/$', AllLinebackersView.as_view()),
+
+    url(r'^defensivebacks/(?P<year>20[0-2][0-9])/$', AllDefensiveBacksView.as_view()),
+
+    url(r'^punters/(?P<year>20[0-2][0-9])/$', AllPuntersView.as_view()),
+
+    # individual offensive players by position, by game, in season
+    url(r'^quarterbacks/(?P<year>20[0-2][0-9])/(?P<name_slug>[a-z_-]+)$', QuarterbackView.as_view()),
+
+    url(r'^runningbacks/(?P<year>20[0-2][0-9])/(?P<name_slug>[a-z_-]+)$', RunningbackView.as_view()),
+
+    url(r'^receivers/(?P<year>20[0-2][0-9])/(?P<name_slug>[a-z_-]+)$', ReceiverView.as_view()),
+
+    url(r'^tightends/(?P<year>20[0-2][0-9])/(?P<name_slug>[a-z_-]+)$', TightEndView.as_view()),
+
+    url(r'^kickers/(?P<year>20[0-2][0-9])/(?P<name_slug>[a-z_-]+)$', KickerView.as_view()),
+
+    # individual defensive player stats by position, by game in season
+    url(r'^linemen/(?P<year>20[0-2][0-9])/(?P<name_slug>[a-z_-]+)$', LinemanView.as_view()),
+
+    url(r'^linebackers/(?P<year>20[0-2][0-9])/(?P<name_slug>[a-z_-]+)$', LinebackerView.as_view()),
+
+    url(r'^defensivebacks/(?P<year>20[0-2][0-9])/(?P<name_slug>[a-z_-]+)$', DefensiveBackView.as_view()),
+
+    url(r'^punters/(?P<year>20[0-2][0-9])/(?P<name_slug>[a-z_-]+)$', PunterView.as_view()),
+
+    #weekly pickem views
+
+    #weekly scheduled matchups
+    url(r'^(?P<year>20[0-2][0-9])/(?P<week>week-{1}[1]*[0]*[0-7]{1})/matchups/$', WeeklyScheduleView.as_view()),
+
+    #weekly scores
+    url(r'^(?P<year>20[0-2][0-9])/(?P<week>week-{1}[1]*[0]*[0-7]{1})/scores/$', WeeklyScoresView.as_view()),
+
+    #winning teams
+    url(r'^(?P<year>20[0-2][0-9])/(?P<week>week-{1}[1]*[0]*[0-7]{1})/winners/$', WeeklyWinnersView.as_view()),
+
+    #losing teams
+    url(r'^(?P<year>20[0-2][0-9])/(?P<week>week-{1}[1]*[0]*[0-7]{1})/losers/$', WeeklyLosersView.as_view()),
 )
